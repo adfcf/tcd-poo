0200f17d062540c9ab27ff50c235b2a8b68a5e29
Alexandre F <69996873+adfcf@users.noreply.github.com>
2023-06-16 10:34:43 -0300

    Update Database.java

--- src/main/java/br/edu/wiedza/db/Database.java
+++ src/main/java/br/edu/wiedza/db/Database.java
@@ -43,8 +43,8 @@ public final class Database {
                 + "&serverTimezone=UTC"
                 + "&autoReconnect=true";
     
-    public static final String USER = "wiedza";
-    public static final String PASSWORD = "4HCBHSJ5vLezd[fL";
+    public static final String USER = "";
+    public static final String PASSWORD = "";
     
     public static Connection getConnection() {
         if (connection == null) {
